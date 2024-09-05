d249b329cf7724cc7c27c1da647b0787dd86b1f8
swapnilaanam <swapnilaanam211@gmail.com>
2024-09-05 15:46:34 +0600

    made some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ToggleBar from "@/components/ToggleBar";
 const Home = () => {
   return (
     <main className="w-full min-h-screen" style={{ background: "linear-gradient(180deg, #373E44 -100%, #191B1F 100%" }}>
-      <div className="flex justify-end items-start pt-[96px] pr-[86px]">
+      <div className="flex justify-end items-start pt-[96px] px-4 lg:pr-[86px]">
         <section className="flex flex-col">
           <ToggleBar />
           <div className="mt-[21px] mb-[17px] mx-auto divider w-[612px] h-[4px] rounded-[2.46px]"></div>
